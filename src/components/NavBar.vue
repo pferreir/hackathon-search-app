b3c40c7231a2fb585f93a0beae61ca041cb2c2be
Pedro Ferreira <pedro.ferreira@cern.ch>
2018-02-20 16:54:27 +0100

    Nicer title

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,15 @@
 <template>
-  <nav class="navbar is-transparent">
-  <div class="navbar-end">
-    <div class="navbar-item">
-      <div class="field is-grouped">
-      <SearchBox />
+  <nav class="navbar is-primary">
+    <h1 class="navbar-item has-text-white subtitle">
+      Amazing search appliance
+    </h1>
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="field is-grouped">
+        <SearchBox />
+        </div>
       </div>
     </div>
-  </div>
   </nav>
 </template>
 
